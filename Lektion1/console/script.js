afaa5175b6a7465f526aa37797d915302bd9ba20
Emma Norling <emma.norling@gmail.com>
2019-11-18 13:45:06 +0100

    Lektion 1

---
+++ Lektion1/console/script.js
@@ -0,0 +1,56 @@
+// En kommentar - per rad 
+/* En kommentar -styckes kommentarer */ 
+
+/*
+märk-språk vs skript-språk  vs progammeringsspråk vs 
+
+märk-språk
+Talar om meta-data:
+HTML CSS XML 
+
+skript-språk
+behöver ej kompileras:
+JavaScript
+PowerShell
+cmdLets
+(python)
+
+programmeringsspråk
+C
+C++
+C#
+Java
+(python)
+
+Beror på vad man har för behov och använda rätt språk till rätt sak.
+Java är inte JavaScript (mer släkt med C#)
+-------------------------------------------------------------------
+Högnivå språk (mellan användaren och systemet) enkla att jobba med
+-------------------------------------------------------------------
+(JavaScript)
+Python
+C# / Java
+Delphi
+-------------------------------------------------------------------
+Lågnivå språk (mellan systemet och hårdvaran)
+-------------------------------------------------------------------
+C++
+C
+Cobol -trafiklysen
+Ansembler
+Mikrokod
+-------------------------------------------------------------------
+
+*/
+
+//skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text';
+
+//skriver ut i konsolen
+console.log('Här skriver vi ut nått i konsolen');
+console.warn('Detta är en varning!');
+console.error('Detta är ett felmeddelande!');
+
+//SANDBOX -DEVELOPER - Här loggar vi allt!
+//PRODUCTION -SKARPT LÄGE - Här loggar vi väldigt lite!
+
