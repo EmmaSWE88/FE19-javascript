2f53de7a26a122a1b29d62fdbe8c2f8e9e409a61
Emma Norling <emma.norling@gmail.com>
2019-11-25 10:28:01 +0100

    Lektion 3

---
+++ Lektion3/selectors/script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+// }); (Ett annat sätt att skriva det på)
+
+$(function(){
+
+   // $("") funkar men enkel-fnuttar är bäst praxis och man bör inte blanda.
+    //$('#btnClick1').click(function(){ //det här är en self-caling function, den anropar sig själv
+     //   console.log('Tryckte på knappen');
+        
+   // })
+
+  // $('#btnClick1').click(() => { console.log('Tryckte på knappen');}); //Om man bara ska ha en grej går detta bra
+   
+$('#btnClick1').click(() => {
+   $('*').hide();
+});
+
+
+
+}); //RÖR EJ
