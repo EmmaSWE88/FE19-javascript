067eb245db17235f53090a51cb6ed17665227431
Emma Norling <emma.norling@gmail.com>
2019-11-27 15:24:06 +0100

    Lektion4

--- Lektion4/classes/script.js
+++ Lektion4/classes/script.js
@@ -1,53 +1,104 @@
 $(function() {
 
-//OBJECT - kan vara helt olika, ställer till problem
+    //OBJECTS -används när vi bara behöver skapa ett objekt
+    const jsObj = {}
 
-    const obj1 = {
-        firstName = 'Object',
-        lastName = 'Object'
-    }
+    //OBJECTS - kan ha olika properties i sig, samt funktioner
+    const obj = {
+        firstName: 'Hans',
+        lastName: 'Mattin-Lassei',
+        fullName: function(){
+            return `${this.firstName} ${this.lastName}`
+        }
+    };
 
-    const obj2 = {
-        first = 'Object',
-        last = 'Object'
-    }
 
+    // CLASSES - används när vi vill skapa flera objekt av samma typ/mall
 
-    //CLASS = Model - är en gemensam standard på hur ett objet ska se ut
+    class JsClass {}
 
-    //Gammalt sätt att bygga klasser -typ som funktioner
-    function person(firstname, lastname) {
-        this.firstName = firstname
-        this.lastName = lastname
-        this.fullName = function() {
-            return this.firstName + ' ' + this.lastName
+    //CLASSES - class är egentligen bara en genväg för prototypen av constructor
+    // (mer populär numera för det följer mer traditionell kodstruktur som andra språk har)
+
+    class ProductClass {
+        constructor(sku, name, description, price, tac) {
+            this.sku = sku//SKU (stock keeping unit)
+            this.name   = name
+            this.description  = description
+            this.price = price
+            this.tax = tax
+        }
+        totalPrice() {
+           return this.price + this.tax
         }
     }
 
-let p1 = new person ('Hans' , 'Mattin-Lassei')
-//document.write(p1.fullName());
+    //PROTOTYPE - så här ser en klass ut i grund och botten
+    //Kallas prototyp-modellen som typ hela JS bygger på 
 
-    //I och med E6 kom dock class (mer som c#)
+    function ProductPrototype(sku, name, description,price,tax) {
+        this.sku = sku
+        this.name = name
+        this.description = description
+        this.price = price
+        this.tax = tax
+        this.totalPrice = function() {
+            return this.price + this.tax
+        }
 
-   class person {
-       constructor(name) {
-            this.fullName = fullname;
-            this.firstName = function() {
-               return fullname.split (' ') [0]
-           }
-           this.lastName = function() {
-               return fullname.split(' ')[1]
-           }
-       }
-   }
-//    constructor(firstname, lastname) {
-//     this.firstName = firstname;
-//     this.lastName = lastname;
-//     this.fullName = function() {
-//         return this.firstName + ' ' + this.lastName
-//     }
+    }
 
-    let user1 = new person ('hans', 'mattin-lassei')
-    document.write(user1.fullName());
+    let product1 = new ProductClass()
+    let product2 = new ProductPrototype()
+
+    const products = []
+
+    for(i=0; i<10; i++) {
+        let sku = `sku-${i}`
+        let name = `product-${i}`
+        let description = 'this is a product'
+        let price = 10 + i;
+        let tax = 25;
+
+        products[i] = new ProductClass(sku,name,description,price,tax);
+    }
+
+    for(product of products) {
+        // console.log(`Product: ${product.name}, Total Price: ${product.totalPrice()}`);
+        // console.log(product.showDemo());
+        
+        
+    }
+
+    //EXTENDS - arv något ärver från en huvudklass
+
+    // class Computer extends ProductClass {
+        // constructor(specification) {
+        //     super();
+
+        //     this.specification = specification
+        // }
+    //     showDemo() {
+    //         console.log('showing demo');
+            
+    //     }
+    // }
+
+    // let c1 = new Computer();
+    // c1.showDemo();
+
+    class Computer {
+        constructor(name,price,brand,specification) {
+            this.name = name
+            this.price = price
+            this.brand = brand
+            this.specification = specification
+        }
+    }
+    showProductInfo(){
+        console.log(`Product: ${this.name}, Price:${this.price}, Specification: ${this.specification}`);
+        
+    }
+
 
 }); //RÖR EJ
---
+++ Lektion4/forms/script.js
@@ -0,0 +1,34 @@
+$(function(){
+
+    function isEmpty(id) {
+        if($(id).val() === '') {
+            $(id).addClass('is-invalid');
+           // $('#firstName > span').removeClass('d-none');
+            $(id).focus();
+        } else {
+            $(id).removeClass('is-invalid');
+            $(id).addClass('is-valid');
+        }
+
+    }
+
+    $('input').keyup(function(e) { //key up på någon av den och den validerar allt samtidigt
+     let id ='#' + e.currentTarget.id
+        validateInput(id);
+        
+    }); //kanske använda this eller en event del för att trigga rätt ställen i formuläret
+
+    $('#regForm').submit((e) => {
+        e.preventDefault();
+
+        validateInput('#firstName');
+        validateInput('#lastName');
+        
+    })
+
+    $('#firstName').on('keyup', function(e){
+        validateEmail(e.currentTarget.value)
+    })
+
+
+}); //DON'T TOUCH THIS!
