03a165ce50159486654ceaa5ace6db2f9a31891a
Emma Norling <emma.norling@gmail.com>
2019-11-30 20:06:29 +0100

    JS inlämning 1 test

--- JS_Inlämning_1/Test/script.js
+++ JS_Inlämning_1/Test/script.js
@@ -29,6 +29,51 @@ $(function() {
         return (false)
     }
 
+    function passLength(){
+        let passLength = document.getElementById('inputPassword');
+        let badColor = "#DC3545";
+        let fontSize = "11.2px";
+        let notStrong = $('#notStrong');
+
+        if(passLength.value.length >= 8 ) {
+            $('#inputPassword').addClass('is-valid')
+            $('#inputPassword').removeClass('is-invalid')
+            return(true)
+            
+        } else {
+            $('#inputPassword').removeClass('is-valid')
+            notStrong.css('color', badColor, 'font-size', fontSize).html("Lösenordet måste vara minst 8 tecken långt.");
+            
+            return(false)
+
+        }
+    }
+   function checkPass() {
+       let password = document.getElementById('inputPassword');
+       let confirm = document.getElementById('confirmPassword');
+       if(password.value == confirm.value && password.value !='') {
+        $('#confirmPassword').removeClass('is-invalid')
+           $('#confirmPassword').addClass('is-valid')
+           return(true)
+       }else {
+        $('#confirmPassword').removeClass('is-valid')
+        $('#confirmPassword').addClass('is-invalid')
+        return(false)
+       }
+   }
+
+   function inCheck() {
+    let inCheck = document.getElementById('consentCheck');
+       if(inCheck == checked){
+           $('#consentCheck').removeClass('is-invalid')
+           $('#consentCheck').addClass('is-valid')
+           return(true)
+       } else {
+        $('#consentCheck').addClass('is-invalid')
+        $('#consentCheck').removeClass('is-valid')
+        return(false)
+       }
+   }
     $('#regForm').submit((e) => {
         e.preventDefault();
 
@@ -38,9 +83,12 @@ $(function() {
         isEmpty('#inputPhoneNumber')
         isEmpty('#inputAddress')
         isEmpty('#inputPassword')
-     
+        isEmpty('#confirmPassword')
+
         ValidateEmail() 
-       
+        passLength()
+        checkPass()
+        inCheck()
         
     })
 
