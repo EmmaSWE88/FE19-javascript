75d43437c5d2bd5dbb4c0acc888cc483a6aacbc2
Emma Norling <emma.norling@gmail.com>
2019-12-05 14:15:51 +0100

    homework js

---
+++ Homework/Skillshare & Udemy/demo-5/script.js
@@ -0,0 +1,10 @@
+//(Repetition efter byte till Udemy)
+
+//FUNCTIONS - declare, invoke, return,parameters/arguments,assign,expression;
+
+function add() {
+    let result = 10 + 4;
+}
+
+const first = add();
+console.log(first);
--- Lektion6/thai-demo/script.js
+++ Lektion6/thai-demo/script.js
@@ -1,19 +1,60 @@
+// const url = 'https://thaikokitallkrogen.azurewebsites.net/api/fooditems'
+// let result = document.getElementById('result')
+// let search = document.getElementById('search')
+// let input = document.getElementById('fooditem')
+// let category = document.getElementById('category')
+
+// search.addEventListener('click', function(e) {
+    
+//     let value = input.value
+//     let cat = category.value
+
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+
+//         for(let item of data.categories[cat].fooditems) {          
+//             if(item.number == value) {
+//                 result.innerHTML = `<p>#${item.number} - ${item.name}</p><p>${item.description}</p>`
+//             }
+//         }
+
+//     })
+// })
+
 const url = 'https://thaikokitallkrogen.azurewebsites.net/api/fooditems'
 let result = document.getElementById('result')
 let search = document.getElementById('search')
 let input = document.getElementById('fooditem')
-let category = document.getElementById('category')
+let select = document.getElementById("selectCategory"); 
+
+function populateCategories() {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        
+        for(let i=0; i < data.categories.length; i++) {
+            let element = document.createElement("option");
+            element.textContent = data.categories[i].name;
+            element.value = i;
+            select.appendChild(element);
+        }
+
+    })
+}
+
+populateCategories();
 
 search.addEventListener('click', function(e) {
     
     let value = input.value
-    let cat = category.value
+    let cat = select.value
 
     fetch(url)
     .then(res => res.json())
     .then(data => {
 
-        for(let item of data.categories[cat].fooditems) {          
+        for(let item of data.categories[cat].fooditems) {       
             if(item.number == value) {
                 result.innerHTML = `<p>#${item.number} - ${item.name}</p><p>${item.description}</p>`
             }
