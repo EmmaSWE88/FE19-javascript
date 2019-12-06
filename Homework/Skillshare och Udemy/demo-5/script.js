c5517d33ae7072b6b6b456a663f288e7624994dd
Emma Norling <emma.norling@gmail.com>
2019-12-06 12:46:35 +0100

    Homework Udemy JS continue

--- Homework/Skillshare och Udemy/demo-5/script.js
+++ Homework/Skillshare och Udemy/demo-5/script.js
@@ -146,15 +146,235 @@
 //Backtick characters `` - above tab (left from one)
 //Interpolation ${} - insert expression(value)
 
-const name = 'john';
-const age = 25;
-const sentence = 'Hey it is ' + name + ' and he is ' + age + ' years old.';
+// const name = 'john';
+// const age = 25;
+// const sentence = 'Hey it is ' + name + ' and he is ' + age + ' years old. And here is some simple math ${}';
 
-const value = `Hey it's ${name} and he is ${age} years old.`;
+// const value = `Hey it's ${name} and he is ${age} years old.`;
 
-console.log(value);
-console.log(sentence);
+// console.log(value);
+// console.log(sentence);
+
+//Array Properties and Methods
+
+// let names = ['john', 'bob','barry','olga','ben', 12, 3];
+
+// //length
+// console.log(names.length);
+// console.log(names[4]);
+// console.log(names[names.length - 1]);
+
+// //concat
+// const lastNames = ['pepper','onion','banana'];
+// const allNames = names.concat(lastNames);
+// console.log(allNames);
+
+// //reverse
+// console.log(allNames.reverse());
+
+// //unshift
+// allNames.unshift('suzy');
+// console.log(allNames);
+
+// //shift
+// allNames.shift();
+// // allNames.shift();
+// console.log(allNames);
+
+// //push
+// allNames.push('suzy');
+// console.log(allNames);
+
+// //pop
+// allNames.pop();
+// console.log(allNames);
+
+// //splice
+// const specificNames = allNames.splice(2,4);
+// console.log(specificNames);
+// console.log(allNames);
+
+//Arrays and For loop
+// let names = ['anna', 'susy','bob'];
+// let newArr = [];
+
+// for(let i = 0; i<names.length; i++){
+//     console.log(i);
+//     console.log(names[i]);
+//     newArr.push(names[i]);
+    
+// }
+// console.log(names);
+// console.log(newArr);
+
+//Functions, return, if, arrays, for loop
+// const names = ['anna','susy','bob'];
+
+// function testing(arr){
+//    // console.log(arr); 
+//    let newArr = [];
+//    for(let i = 0; i<arr.length; i++){
+//        let newItem = `${arr[i]} jordan`
+//        newArr.push(newItem)
+// }
+// if(newArr.length > 2){
+//     return console.log(`here is your long array : ${newArr}`);
+    
+//     }
+// console.log('this code will still be executed');
+// }
 
 
+// testing(names);
+//=====================================================================
+//Primitive Data Types:
+//String, Number, Symbol, Boolean, Undefined, Null
+//Everything else: Arrays,Functions,Objects = object
+//(typeof)
 
+//assigning a primitive data type value to a variable:
+//any changes are made directly to that value, without affecting 
+//the original value.
+
+//assigning a non-primitieve data type value to a variable, is done by
+//reference so any changes will affect ALL the references
+
+// let number = 1;
+// let number2 = number;
+// number2 = 7;
+
+// console.log(`the first value is ${number}`);
+// console.log(`the second value is ${number2}`);
+
+// let person = { name: 'bob' };
+// let person2 = person;
+// person2.name = 'susy';
+
+// console.log(`the name of the first person ${person.name}`);
+// console.log(`the name of the second person ${person2.name}`);
+
+//============================================================
+//Null and Undefined
+
+//Both represent "no value"
+
+//Undefined - "javascript can not find a value for this"
+
+//variable without value
+//missing function parameters
+//missing object property
+
+//Null -"developer sets value"
+
+// let number = 20 + null; //20 + 0 
+// console.log(number);
+
+// let number2 = 20 + undefined; //
+// console.log(number2);
+
+//============================================
+//Trythy vs Falsy
+//Falsy:  "", '', ``, 0, -0, Nan, false, null,undefined
+
+// const bool1 = false;
+// const result = 2 < 5;
+// console.log(typeof bool1);
+// console.log(result);
+
+// const text = 0;
+
+// if(text){
+//     console.log('runs if true');
+// }
+// else {
+//     console.log('runs if false');
+// }
+
+//====================================
+//Unary operator - typeof
+// let text = 'some text';
+// console.log(typeof text); //operand, only needs one operand, therefore unary
+// //Binary operator - you need two operands, therefore binary
+// let number = 3;
+// let number2 = 2 + 5;
+
+//Ternary operator
+//condition ? (runs if true) : (runs if false)
+
+// let value = 6 < 5;
+
+// value ?  console.log('value is true') :  console.log('value is false'); //Ternary Operator
+
+// if(value) {
+//     console.log('value is true');
+// }
+// else {
+//     console.log('value is false');  
+// }
+
+//=============================================================================
+// GLOBAL SCOPE vs LOCAL SCOPE (Part 1)
+
+//Any variable outside the code block is said to be in GLOBAL SCOPE
+//A GLOBAL SCOPE can be accessed anywhere in the program
+//PROBLEMS : name collisions, modify by MISTAKE
+
+// let name = 'bob';
+// name = 'peter';
+
+// function calculate () {
+//     //some other code ...
+//     console.log(name);
+//     //by mistake
+//     name = 'orange';
+//     function inner(){
+//         name = 'this is some other value';
+//         console.log(`this is from inner function ${name}`);
+        
+//     }
+//     inner();
+// }
+// calculate();
+
+// if(true){
+//      //some other code ...
+//      console.log(name);
+//      //by mistake
+//      name = 'pants';
+// }
+
+// console.log(`my name is ${name} and I'm awesome!`);
+
+//=============================================================================
+// GLOBAL SCOPE vs LOCAL SCOPE (Part 2)
+
+//LOCAL SCOPE
+//Can NOT be accessed from outside of the code blocks (curly braces)
+//NOT VAR (const and let)
+//USE KEYWORDS!
+
+// let name = 'bob';
+
+// function calculate (){
+//     const name = 'susy';
+//     const age = 30;
+//     //variable without keyword
+//     becomesGlobal = 'global value'
+// }
+// calculate();
+
+// if (true) {
+//     const name = 'anna';
+// }
+// console.log(name);
+// console.log(becomesGlobal);
+
+
+// //console.log(age);
+
+// console.log(`my name is ${name} and I'm awesome`);
+
+//======================================================
+// Variable Lookup
+// { } - code block
 
