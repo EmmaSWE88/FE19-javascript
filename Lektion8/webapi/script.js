a436418bc0b9b8816d5a9c868800166fe67f6b83
Emma Norling <emma.norling@gmail.com>
2019-12-11 10:17:52 +0100

    Lektion 8

---
+++ Lektion8/webapi/script.js
@@ -0,0 +1,24 @@
+function getFullName(firstname, lastname) {
+    fetch(`https://inlupp-fa.azurewebsites.net/api/users?firstname=${firstname}&lastname=${lastname}`)
+    .then(res => res.text())
+    .then(data => console.log(data))
+}
+function getTotalSales(){ //kör på den här varianten tex
+    fetch(`https://inlupp-fa.azurewebsites.net/api/total-sales`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        localStorage.setItem('totalSales', data)
+
+        //här gör jag det jag vill göra (relaterat till uppgiften bland annat)
+    }) 
+
+}
+
+let totalSales = JSON.parse(localStorage.getItem('totalSales'));
+console.log(totalSales.currency);
+console.log(totalSales.amount);
+
+//getFullName('Hans', 'ML')
+getTotalSales()
+
