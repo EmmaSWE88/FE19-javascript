cb848c78ad6810843e372719b0ac03e72aff2229
Emma Norling <emma.norling@gmail.com>
2019-12-11 08:57:28 +0100

    Udemy JS contin

---
+++ Homework/Skillshare och Udemy/demo-6/script.js
@@ -0,0 +1,8 @@
+//Callback Functions, Higherorder Functions, Functions as First Class Object
+
+//Functions are first class objects - stored in a variable, passed as an argument to
+//another function, return from function (closure)
+//Higher Order Function - accepts another function as argument or returns another 
+//function as a result
+//Callback Function - passed  to another function as an argument and executed inside that
+//function
