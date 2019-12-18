c3926a76ca6340ca3e631090f64144c86ad7c0e3
Emma Norling <emma.norling@gmail.com>
2019-12-18 09:31:18 +0100

    JS uppgift2 api påläsning

--- Lektion6/thai-demo-2/script.js
+++ Lektion6/thai-demo-2/script.js
@@ -2,29 +2,35 @@ const url = 'https://thaikokitallkrogen.azurewebsites.net/api/fooditems'
 let result = document.getElementById('result')
 let search = document.getElementById('search')
 let input = document.getElementById('fooditem')
-let category = document.getElementById('foodcategory')
+let select = document.getElementById("selectCategory"); 
 
-search.addEventListener('select', function(e){
-    let selection = category.value
+function populateCategories() {
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        for(let item of data.categories[selection].fooditems) {          
-          
+        
+        for(let i=0; i < data.categories.length; i++) {
+            let element = document.createElement("option");
+            element.textContent = data.categories[i].name;
+            element.value = i;
+            select.appendChild(element);
         }
-    }
-})
+
+    })
+}
+
+populateCategories();
 
 search.addEventListener('click', function(e) {
     
     let value = input.value
-    let cat = category.querySelector
+    let cat = select.value
 
     fetch(url)
     .then(res => res.json())
     .then(data => {
 
-        for(let item of data.categories[cat].fooditems) {          
+        for(let item of data.categories[cat].fooditems) {       
             if(item.number == value) {
                 result.innerHTML = `<p>#${item.number} - ${item.name}</p><p>${item.description}</p>`
             }
@@ -32,3 +38,38 @@ search.addEventListener('click', function(e) {
 
     })
 })
+
+// const url = 'https://thaikokitallkrogen.azurewebsites.net/api/fooditems'
+// let result = document.getElementById('result')
+// let search = document.getElementById('search')
+// let input = document.getElementById('fooditem')
+// let category = document.getElementById('foodcategory')
+
+// search.addEventListener('select', function(e){
+//     let selection = category.value
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//         for(let item of data.categories[selection].fooditems) {          
+          
+//         }
+//     }
+// })
+
+// search.addEventListener('click', function(e) {
+    
+//     let value = input.value
+//     let cat = category.querySelector
+
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+
+//         for(let item of data.categories[cat].fooditems) {          
+//             if(item.number == value) {
+//                 result.innerHTML = `<p>#${item.number} - ${item.name}</p><p>${item.description}</p>`
+//             }
+//         }
+
+//     })
+// })
--- Lektion8/webapi/script.js
+++ Lektion8/webapi/script.js
@@ -1,24 +1,45 @@
-function getFullName(firstname, lastname) {
-    fetch(`https://inlupp-fa.azurewebsites.net/api/users?firstname=${firstname}&lastname=${lastname}`)
-    .then(res => res.text())
-    .then(data => console.log(data))
-}
-function getTotalSales(){ //kör på den här varianten tex
-    fetch(`https://inlupp-fa.azurewebsites.net/api/total-sales`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        localStorage.setItem('totalSales', data)
+const messages = document.getElementById('messages');
 
-        //här gör jag det jag vill göra (relaterat till uppgiften bland annat)
-    }) 
+fetch('http://inlupp-fa.azurewebsites.net/api/messages')
+.then(res => res.json())
+.then(data => {
 
-}
+    for(message of data) {
 
-let totalSales = JSON.parse(localStorage.getItem('totalSales'));
-console.log(totalSales.currency);
-console.log(totalSales.amount);
+        messages.insertAdjacentHTML('beforeend', `
+            <a>
+                <p>${message.from}</p>
+                <p>${message.title}</p>
+            </a>
+        `); 
 
-//getFullName('Hans', 'ML')
-getTotalSales()
+    }
+
+
+})
+
+
+// function getFullName(firstname, lastname) {
+//     fetch(`https://inlupp-fa.azurewebsites.net/api/users?firstname=${firstname}&lastname=${lastname}`)
+//     .then(res => res.text())
+//     .then(data => console.log(data))
+// }
+// function getTotalSales(){ //kör på den här varianten tex
+//     fetch(`https://inlupp-fa.azurewebsites.net/api/total-sales`)
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//         localStorage.setItem('totalSales', data)
+
+//         //här gör jag det jag vill göra (relaterat till uppgiften bland annat)
+//     }) 
+
+// }
+
+// let totalSales = JSON.parse(localStorage.getItem('totalSales'));
+// console.log(totalSales.currency);
+// console.log(totalSales.amount);
+
+// //getFullName('Hans', 'ML')
+// getTotalSales()
 
