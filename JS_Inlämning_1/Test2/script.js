b4afa3c638ea57453dd34ecb02f6dbd0c741f587
Emma Norling <emma.norling@gmail.com>
2020-01-06 23:04:57 +0100

    JS uppgift forts

--- JS_Inlämning_1/Test2/script.js
+++ JS_Inlämning_1/Test2/script.js
@@ -1,8 +1,142 @@
-$(function() {
+// $(function() {
 
+//     function isEmpty(id) {
+//         if($(id).val() === '') {
+//             $(id).addClass('is-invalid')
+//             $(id).removeClass('is-valid')
+
+//             $(id).focus();
+//         } else {
+//             $(id).removeClass('is-invalid')
+//             $(id).addClass('is-valid')
+//         }
+//     }
+
+//     $('input').keyup(function(e) {
+//         let id = "#" + e.currentTarget.id
+//         isEmpty(id);
+//     });
+
+//     function ValidateEmail(email) 
+//     {
+//      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($('#inputEmail').val()))
+//       {
+//         $('#inputEmail').removeClass('is-invalid')
+//         $('#inputEmail').addClass('is-valid') 
+//         return (true)
+//       }
+//         $('#inputEmail').addClass('is-invalid')
+//         $('#inputEmail').focus();
+//         return (false)
+//     }
+
+//         $("#inputPassword").blur(function passLength(){
+//             let passLength = document.getElementById('inputPassword');
+//             if(passLength.value.length >= 1 && passLength.value.length < 8 ) {
+//                 $('#inputPassword').removeClass('is-valid')
+//                 $('#inputPassword').addClass('is-invalid')
+                
+//                 return($("#notStrong").html("Lösenordet måste vara minst 8 tecken långt!"))
+                
+//             } 
+//             else{
+//                 $('#inputPassword').addClass('is-valid')
+//                 $('#inputPassword').removeClass('is-invalid')
+//                 return(true)
+//             }
+//         });
+        
+
+//    function checkPass() {
+//        let password = regForm.inputPassword.value;
+//        let confirm = regForm.confirmPassword.value;
+//        if(password.value === confirm.value) {
+//         $('#confirmPassword').removeClass('is-invalid')
+//            $('#confirmPassword').addClass('is-valid')
+//            return(true)
+//        }else {
+//         $('#confirmPassword').removeClass('is-valid')
+//         $('#confirmPassword').addClass('is-invalid')
+//         return (false)
+//        }
+//    }
+
+//    function inCheck() {
+//     let inCheck = document.getElementById('consentCheck');
+//        if(inCheck.checked){
+//            $('#consentCheck').removeClass('is-invalid')
+//            $('#consentCheck').addClass('is-valid')
+//            return(true)
+//        } else {
+//         $('#consentCheck').addClass('is-invalid')
+//         $('#consentCheck').removeClass('is-valid')
+//         return(false)
+//        }
+//    }
+//     $('#regForm').submit((e) => {
+//         e.preventDefault();
+
+//         isEmpty('#firstName');
+//         isEmpty('#lastName');
+//         isEmpty('#inputEmail')
+//         isEmpty('#inputPhoneNumber')
+//         isEmpty('#inputAddress')
+//         isEmpty('#inputPassword')
+//         isEmpty('#confirmPassword')
+//         isEmpty('#inputCity')
+//         isEmpty('#inputZip')
+//         isEmpty('#inputCountry')
+     
+       
+
+//         ValidateEmail() 
+//         checkPass()
+//         passLength()
+//         // checkPassword('#inputPassword')
+//         inCheck()
+
+//     })
+//     const complete = document.querySelectorAll('.is-valid');
+//     if(complete.length >= 11){
+//         document.querySelector('.submitBtn').disabled =false;
+//     }
+//     else {
+//         document.querySelector('.submitBtn').disabled = true;
+//     }
+
+// });
+
+
+//==============================================================================================================
+//==============================================================================================================
+//BOOTSTRAP EXAMPLE
+// Disable form submissions if there are invalid fields
+// (function() {
+//     'use strict';
+//     window.addEventListener('load', function() {
+//       // Get the forms we want to add validation styles to
+//       let forms = document.getElementsByClassName('needs-validation');
+//       // Loop over them and prevent submission
+//       let validation = Array.prototype.filter.call(forms, function(form) {
+//         form.addEventListener('submit', function(event) {
+//           if (form.checkValidity() === false) {
+//             event.preventDefault();
+//             event.stopPropagation();
+//           }
+//           form.classList.add('was-validated');
+//         }, false);
+//       });
+//     }, false);
+
+//   })();
+//==============================================================================================================
+//==============================================================================================================  
+
+$(function(){
     function isEmpty(id) {
         if($(id).val() === '') {
             $(id).addClass('is-invalid')
+            $(id).removeClass('is-valid')
 
             $(id).focus();
         } else {
@@ -29,69 +163,78 @@ $(function() {
         return (false)
     }
 
-        $("#inputPassword").blur(function passLength(){
-            let passLength = document.getElementById('inputPassword');
-            if(passLength.value.length >= 1 && passLength.value.length < 8 ) {
-                $('#inputPassword').removeClass('is-valid')
-                $('#inputPassword').addClass('is-invalid')
-                
-                return($("#notStrong").html("Lösenordet måste vara minst 8 tecken långt!"))
-                
-            } 
-            else{
-                $('#inputPassword').addClass('is-valid')
-                $('#inputPassword').removeClass('is-invalid')
-                return(true)
-            }
-        });
-        
+    function checkPassStrength() {
+    let passStrength = document.getElementById('inputPassword');
+    if(passStrength.value.length >= 8) {
+        length.classList.remove("is-invalid");
+        length.classList.add("is-valid");
+      } else {
+        length.classList.remove("is-valid");
+        length.classList.add("is-invalid");
+      }
+    
+    }
+    function validatePass() { //att matcha lösenorden funkar inte
+        let password = document.getElementById('inputPassword').value;
+        let confirmPassword = document.getElementById('confirmPassword').value;
+        if (password != confirmPassword) {
+            $('#confirmPassword').addClass('is-invalid')
+            document.getElementById('conPassP').innerText = ("Du har inte repeterat samma lösenord!");
+            $('#confirmPassword').removeClass('is-valid')
+            return (false);
+        } else {
+            return (true);
+        }
 
-//    function checkPass() {
-//        let password = regForm.inputPassword.value;
-//        let confirm = regForm.confirmPassword.value;
-//        if(password.value == confirm.value) {
-//         $('#confirmPassword').removeClass('is-invalid')
-//            $('#confirmPassword').addClass('is-valid')
-//            return(true)
-//        }else {
-//         $('#confirmPassword').removeClass('is-valid')
-//         $('#confirmPassword').addClass('is-invalid')
-//         return(false)
-//        }
-//    }
-
-   function inCheck() {
-    let inCheck = document.getElementById('consentCheck');
-       if(consentCheck.checked == true){
+       
+}
+    function inCheck() {
+    let checkComp = document.getElementById('consentCheck');
+       if(checkComp.checked == true){
            $('#consentCheck').removeClass('is-invalid')
            $('#consentCheck').addClass('is-valid')
-           return(true)
+           
        } else {
         $('#consentCheck').addClass('is-invalid')
         $('#consentCheck').removeClass('is-valid')
         return(false)
        }
    }
+
+   function radioCheck() {
+    let radioComp = document.getElementById('customRadio1');
+       if(radioComp.checked == true){
+           $('#customRadio1').removeClass('is-invalid')
+           $('#customRadio1').addClass('is-valid')
+           
+       } else {
+        $('#customRadio1').addClass('is-invalid')
+        $('#customRadio1').removeClass('is-valid')
+        return(false)
+       }
+   }
+
+
     $('#regForm').submit((e) => {
         e.preventDefault();
 
         isEmpty('#firstName');
         isEmpty('#lastName');
-        isEmpty('#inputEmail')
-        isEmpty('#inputPhoneNumber')
-        isEmpty('#inputAddress')
-        isEmpty('#inputPassword')
-        isEmpty('#confirmPassword')
-        isEmpty('#inputCity')
-        isEmpty('#inputZip')
-        isEmpty('#inputCountry')
-       
+        isEmpty('#inputEmail');
+        isEmpty('#inputPhoneNumber');
+        isEmpty('#inputAddress');
+        isEmpty('#inputPassword');
+        isEmpty('#confirmPassword');
+        isEmpty('#inputCity');
+        isEmpty('#inputZip');
+        isEmpty('#inputCountry');
+        isEmpty('#regFormTextarea1');
 
-        ValidateEmail() 
-        passLength()
-        // checkPassword('#inputPassword')
-        inCheck('#consentCheck')
+        checkPassStrength();
+        validatePass();
+        inCheck();
+        radioCheck();
+    });
 
-    })
+}); // DO NOT DELETE 
 
-});
