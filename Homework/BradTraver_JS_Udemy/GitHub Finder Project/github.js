8beb30f0377ab43d51794ca3561cf857c7e1550e
Emma Norling <emma.norling@gmail.com>
2020-01-12 10:31:09 +0100

    Brad JS

---
+++ Homework/BradTraver_JS_Udemy/GitHub Finder Project/github.js
@@ -0,0 +1,15 @@
+class Github {
+    constructor() {
+        this.client_id = '8911c8b17f26620bbfdf';
+        this.client_secret = '11bcbd940201271f7a50f8c5443f8d040385ad7c';
+    }
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+          profile  
+        }
+    }
+}
