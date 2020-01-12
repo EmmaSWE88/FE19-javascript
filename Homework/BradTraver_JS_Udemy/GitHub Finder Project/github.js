c0e12bb8ab60bcdd629223517b87eec70f09dbbc
Emma Norling <emma.norling@gmail.com>
2020-01-12 10:54:09 +0100

    Brad JS Github finder project

--- Homework/BradTraver_JS_Udemy/GitHub Finder Project/github.js
+++ Homework/BradTraver_JS_Udemy/GitHub Finder Project/github.js
@@ -1,15 +1,22 @@
 class Github {
     constructor() {
-        this.client_id = '8911c8b17f26620bbfdf';
-        this.client_secret = '11bcbd940201271f7a50f8c5443f8d040385ad7c';
+      this.client_id = '8911c8b17f26620bbfdf';
+      this.client_secret = '11bcbd940201271f7a50f8c5443f8d040385ad7c';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
     }
+  
     async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
-
-        const profile = await profileResponse.json();
-
-        return {
-          profile  
-        }
+      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const profile = await profileResponse.json();
+      const repos = await repoResponse.json();
+  
+      return {
+        profile,
+        repos
+      }
     }
-}
+  }
--- Homework/BradTraver_JS_Udemy/GitHub Finder Project/ui.js
+++ Homework/BradTraver_JS_Udemy/GitHub Finder Project/ui.js
@@ -1,11 +1,12 @@
 class UI {
-    constructor(){
-        this.profile = document.getElementById('profile');
-    }
-    //Display profile in UI
-    showProfile(user){
-     this.profile.innerHTML = `
-     <div class="card card-body mb-3">
+  constructor() {
+    this.profile = document.getElementById('profile');
+  }
+
+  // Display profile in UI
+  showProfile(user) {
+    this.profile.innerHTML = `
+      <div class="card card-body mb-3">
         <div class="row">
           <div class="col-md-3">
             <img class="img-fluid mb-2" src="${user.avatar_url}">
@@ -28,10 +29,32 @@ class UI {
       </div>
       <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
-     `;
-     
-    }
-    // Show alert message
+    `;
+  }
+  //Show user repos
+  showRepos(repos){
+    let output = '';
+   repos.forEach(function(repo){
+    output += `
+    <div class="card card-body mb-2">
+    <div class="row">
+      <div class="col-md-6">
+       <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+      </div>
+      <div class="col-md-6">
+      <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+      <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+      <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+      </div>
+    </div>
+    </div>
+    `;
+   });
+
+   //Output repos
+   document.getElementById('repos').innerHTML = output;
+  }
+  // Show alert message
   showAlert(message, className) {
     // Clear any remaining alerts
     this.clearAlert();
